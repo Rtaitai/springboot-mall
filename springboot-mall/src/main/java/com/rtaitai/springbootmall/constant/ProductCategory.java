d9bc58b0a1ec687000c5d103fdfb6ee96e87fec1
ted <tedwork52012@outlook.com>
2024-01-29 22:26:59 +0800

    商品功能 - Refactor category，使用 Enum 類型

---
+++ springboot-mall/src/main/java/com/rtaitai/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.rtaitai.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- springboot-mall/src/main/java/com/rtaitai/springbootmall/model/Product.java
+++ springboot-mall/src/main/java/com/rtaitai/springbootmall/model/Product.java
@@ -1,12 +1,16 @@
 package com.rtaitai.springbootmall.model;
 
+import com.rtaitai.springbootmall.constant.ProductCategory;
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -14,75 +18,4 @@ public class Product {
     private Date createdDate;
     private Date lastModifiedDate;
 
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
 }
