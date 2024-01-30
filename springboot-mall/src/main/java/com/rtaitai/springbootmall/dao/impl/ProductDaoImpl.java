ed4a13f64452b56c13b01773b3b92511e22a5938
rtaitai <a0986529036@gmail.com>
2024-01-31 01:24:31 +0800

    商品功能 - 實作查詢商品列表 (查詢條件)

--- springboot-mall/src/main/java/com/rtaitai/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/rtaitai/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,5 +1,6 @@
 package com.rtaitai.springbootmall.dao.impl;
 
+import com.rtaitai.springbootmall.constant.ProductCategory;
 import com.rtaitai.springbootmall.dao.ProductDao;
 import com.rtaitai.springbootmall.dto.ProductRequest;
 import com.rtaitai.springbootmall.model.Product;
@@ -20,11 +21,21 @@ public class ProductDaoImpl implements ProductDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
-    public List<Product> getProducts() {
+    public List<Product> getProducts(ProductCategory category, String search) {
 
-        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, created_date, last_modified_date FROM product ";
+        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, created_date, last_modified_date FROM product WHERE 1=1";
 
         Map<String, Object> map = new HashMap<>();
+
+        if (category !=null) {
+            sql = sql + " AND category = :category";
+            map.put("category", category.name());
+        }
+
+        if (search !=null) {
+            sql = sql + " AND product_name LIKE :search";
+            map.put("search", "%" + search + "%");
+        }
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
