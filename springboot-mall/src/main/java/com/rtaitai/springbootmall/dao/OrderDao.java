76f69317964907e9d1c97cff9adfe17e40507c55
rtaitai <a0986529036@gmail.com>
2024-02-23 02:31:24 +0800

    訂單功能 - 創建訂單(上)

---
+++ springboot-mall/src/main/java/com/rtaitai/springbootmall/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package com.rtaitai.springbootmall.dao;
+
+import com.rtaitai.springbootmall.model.OrderItem;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    Integer createOrder(Integer userId, Integer totalAmount);
+
+    void createOrderItems(Integer orderId, List<OrderItem> orderItemList);
+}
---
+++ springboot-mall/src/main/java/com/rtaitai/springbootmall/model/OrderItem.java
@@ -0,0 +1,17 @@
+package com.rtaitai.springbootmall.model;
+
+import lombok.Data;
+
+@Data
+public class OrderItem {
+
+    private Integer orderItemId;
+
+    private Integer orderId;
+
+    private Integer productId;
+
+    private Integer quantity;
+
+    private Integer amount;
+}
